fd4fd8cece108f78a6ea3d80e42301025db5e6f5
yourmomahegao <mrdumbs70@gmail.com>
2023-11-20 16:59:10 +0200

    Creation

---
+++ ProductScan/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace ProductScan
+{
+    public static class Constants
+    {
+        public static string fasConnection = "data source=192.168.180.8\\flat;initial catalog=FAS;persist security info=True;user id=CTS_SOFT;password=wnc_ghju;MultipleActiveResultSets=True;TrustServerCertificate=true;";
+        public static string smdConnection = "data source=192.168.180.8\\flat;initial catalog=SMDCOMPONETS;persist security info=True;user id=CTS_SOFT;password=wnc_ghju;MultipleActiveResultSets=True;TrustServerCertificate=true;";
+    }
+}
